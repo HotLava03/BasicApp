d8509a767fd27c6a9b5894d22fd7e1b19d1bfd7a
Toba Ojo <71614914+Toba-O@users.noreply.github.com>
2020-10-18 15:18:04 -0400

    Added the needed semicolons here

--- Addition.java
+++ Addition.java
@@ -7,12 +7,12 @@ public class Addition{
         Scanner scan = new Scanner(System.in);
 
 
-        /* Prompt *
+        /* Prompt */
         System.out.println("Hello, welcome to the Integer Addition Calculator");
         System.out.println("To start, input two integers below\n);
 
-        x = scan.nextInt()
-        y = scan.nextInt()
+        x = scan.nextInt();
+        y = scan.nextInt();
         System.out.println(Your sum is x+y);
     }
 }
