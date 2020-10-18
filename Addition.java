9ed1101bf36f4b37323c9d7dafd98b74eefee470
Mantee <29049920+Manteezs@users.noreply.github.com>
2020-10-18 15:06:51 -0400

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -7,13 +7,13 @@ public class Addition{
         Scanner scan = new Scanner(System.in);
 
 
-        /* Prompt */
+        /* Prompt *
         System.out.println("Hello, welcome to the Integer Addition Calculator");
-        System.out.println("To start, input two integers below\n");
+        System.out.println("To start, input two integers below\n);
 
-        x = scan.nextInt();
-        y = scan.nextInt();
-        System.out.println("Your sum is x+y");
+        x = scan.nextInt()
+        y = scan.nextInt()
+        System.out.println(Your sum is x+y);
     }
 }
   
